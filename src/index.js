8aafa2897527e5e27aaa069237fb98583e4f679c
DavidB24 <david.barbu@s.unibuc.ro>
2021-12-09 14:30:13 +0200

    update1

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const {handleGreeting, otherValue} = require('./greeting');
 const app = express();
 const port = 3000;
 
+app.use((req, res, next)=>{
+    console.log('Time:', Date.now());
+    //next()
+});
+
 app.get("/", (request,response)=>{
     response.send("Hello World!!!!");
 })
